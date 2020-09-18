8b460f7050cf3d2525b1b193dcbf16cfe431c146
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-09-18 23:43:21 +0900

    :white_check_mark: (test code create)

---
+++ tests/Feature/Database/DatabaseTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\Database;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->attributes = [
+            'title' => 'テスト太郎',
+            'author' => 'さくしゃ',
+        ];
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(
+            Schema::hasColumns('books', [
+                'id', 'title', 'author'
+            ]),
+            1
+        );
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_CreateDate()
+    {
+        Book::create($this->attributes);
+        $this->assertDatabaseHas('books', $this->attributes);
+    }
+
+    public function test_CreateDateCheck()
+    {
+        $title = [
+            'title' => "TEST",
+            'author' => "TESTAUTHOR"
+        ];
+        Book::insert($title);
+        $this->assertDatabaseHas('books', [
+            'title' => "TEST"
+        ]);
+    }
+
+
+}
